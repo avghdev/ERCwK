86ce8872f84055803828e4216b77f8e853821117
Dumplin <avghdev@gmail.com>
2021-06-22 09:30:08 -0700

    Initial Commit

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    public GameObject obstPrefab;
+    GroundSpawner groundSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+    }
+    
+    void SpawnObstacle() {
+        int obstacleSpawnIndex = Random.Range(2,5);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+        Instantiate(obstPrefab, spawnPoint.position, Quaternion.identity, transform);
+    }
+    
+    void OnTriggerExit(Collider other) {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public float horMultiplier = 2;
+    public Rigidbody rb;
+    
+    float horInput;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void FixedUpdate() {
+        Vector3 moveForward = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horMove = transform.right * speed * Time.fixedDeltaTime * horInput;
+        rb.MovePosition(rb.position + moveForward + horMove);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horInput = Input.GetAxis("Horizontal");
+    }
+}
